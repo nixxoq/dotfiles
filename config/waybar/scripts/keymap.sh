07d928fa7cd1edafc97470ed578a95261535ca87
nixxoq <nixxoq@outlook.com>
2024-10-23 17:59:52 +0300

    Changes:
    - Redesigned waybar style to make  it look cleaner and better
    - Removed keymap.sh script from waybar/scripts folder (unused)
    - Using Windows + L key for wlogout (logout screen)
    - Restructured the windowrules config

--- config/waybar/scripts/keymap.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-# Получение текущей активной раскладки клавиатуры
-keymap=$(hyprctl devices -j | jq -r '.keyboards[] | select(.main == true) | .active_keymap')
-
-# Вывод результата для Waybar
-echo "$keymap"
