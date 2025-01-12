a728e441d71c3d6d4d31f421bf7a7edb72350b04
nixxoq <nixxoq@outlook.com>
2025-01-12 16:46:12 +0200

    fix hyprlock crashing after executing `systemctl suspend` command

--- config/hypr/scripts/StartUp.sh
+++ config/hypr/scripts/StartUp.sh
@@ -4,6 +4,8 @@ if lspci | grep -i nvidia > /dev/null; then
     env LIBVA_DRIVER_NAME=nvidia
     env GDM_BACKEND=nvidia-drm
     env __GLX_VENDOR_LIBRARY_NAME=nvidia
+    env VDPAU_DRIVER,nvidia
+    env NVD_BACKEND,direct
 else
     env LIBVA_DRIVER_NAME=radeonsi
     env GDM_BACKEND=amd
